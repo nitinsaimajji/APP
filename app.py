8dc31b62b6cee3ff3328dc30d774bc9c0a74b7d3
Nitin Sai Majji <nitin41202@gmail.com>
2024-06-04 19:15:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import re
+!pip install tensorflow
 import tensorflow as tf
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
